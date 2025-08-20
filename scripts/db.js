3f6017945c127ef1363afce7227515f96cc3a74e
imra.s <i.skaliks.is@gmail.com>
2025-08-20 15:22:35 +0200

    create json and add image

---
+++ scripts/db.js
@@ -0,0 +1,11 @@
+let dishes = [
+  {
+    'name': 'Sommer Rollen',
+    'price': 12.99,
+    'image': './assets/img/summer_roll.jpg',
+    'description':
+      'Frisches Gemüse mit süß-sauerer Soße und Hühnchen, eingewickelt in Reispapier',
+  },
+];
+
+let basket = [];
