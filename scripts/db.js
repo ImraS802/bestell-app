ff51c00a03a77e17e95292f303a0db53e02c025a
imra.s <i.skaliks.is@gmail.com>
2025-08-21 21:14:28 +0200

    feat: add font and main dishes

--- scripts/db.js
+++ scripts/db.js
@@ -1,10 +1,30 @@
 let dishes = [
   {
     'name': 'Sommer Rollen',
-    'price': 12.99,
+    'price': 6.5,
     'image': './assets/img/summer_roll.jpg',
     'description':
       'Sommerrolle mit Reisnudeln, geriebenen Möhren, Gurke, Mango, Kräutern, Hoisinsauce und Zutat nach Wahl',
+  },
+  {
+    'name': 'Banh Mi Xiu Mai',
+    'price': 11.99,
+    'image': './assets/img/summer_roll.jpg',
+    'description':
+      'vietnamesisches Baguette mit Garnelenfleischbällchen, Tomaten, Lauchzwiebeln und Koriander',
+  },
+  {
+    'name': 'Knusprige Wantan',
+    'price': 6.5,
+    'image': './assets/img/summer_roll.jpg',
+    'description': 'Garnelen, Kartoffeln, Gemüse und hausgemachter Sauce',
+  },
+  {
+    'name': 'Pho Tron Hanoi',
+    'price': 13.5,
+    'image': './assets/img/summer_roll.jpg',
+    'description':
+      'Reisbandnudeln mit hausgemachter Sauce, frischen Kräutern, Zutat nach Wahl und verschiedenen Toppings',
   },
 ];
 
