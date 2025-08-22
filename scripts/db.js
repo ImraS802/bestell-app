6b3f214c197e119a8814429b2ee42cf0984b2721
imra.s <i.skaliks.is@gmail.com>
2025-08-22 15:36:01 +0200

    feat: split hero image and add headlines

--- scripts/db.js
+++ scripts/db.js
@@ -1,31 +1,44 @@
-let dishes = [
+let mainDishes = [
   {
     'name': 'Sommer Rollen',
     'price': 6.5,
-    'image': './assets/img/summer_roll.jpg',
     'description':
       'Sommerrolle mit Reisnudeln, geriebenen Möhren, Gurke, Mango, Kräutern, Hoisinsauce und Zutat nach Wahl',
   },
   {
     'name': 'Banh Mi Xiu Mai',
     'price': 11.99,
-    'image': './assets/img/summer_roll.jpg',
     'description':
       'vietnamesisches Baguette mit Garnelenfleischbällchen, Tomaten, Lauchzwiebeln und Koriander',
   },
   {
     'name': 'Knusprige Wantan',
     'price': 6.5,
-    'image': './assets/img/summer_roll.jpg',
     'description': 'Garnelen, Kartoffeln, Gemüse und hausgemachter Sauce',
   },
   {
     'name': 'Pho Tron Hanoi',
     'price': 13.5,
-    'image': './assets/img/summer_roll.jpg',
     'description':
       'Reisbandnudeln mit hausgemachter Sauce, frischen Kräutern, Zutat nach Wahl und verschiedenen Toppings',
   },
 ];
 
 let basket = [];
+
+function init() {
+  rendermainDishes();
+}
+
+function rendermainDishes() {
+  let mainDishRef = document.getElementById('content');
+  mainDishRef.innerHTML = '';
+
+  for (let i = 0; i < mainDishes.length; i++) {
+    renderSingleMainDish(mainDishRef, i);
+  }
+}
+
+function renderSingleMainDish(mainDishRef, index) {
+  mainDishRef.innerHTML += getHTMLForMainDishTemplate(index);
+}
---
+++ scripts/template.js
@@ -0,0 +1,8 @@
+function getHTMLForMainDishTemplate(indexMainDish) {
+  return `
+    <div class="food_card">
+    <h3 id="name"></h3>
+    <div id="description"></div>
+    <div id="price"></div>
+    </div>`;
+}
